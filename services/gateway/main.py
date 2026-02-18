429e3c7214ce6c8fca949d8abfc6ce6be4089907
andrey.lesnov <andreythecoolguy@gmail.com>
2026-02-18 14:48:58 +0300

    feat: add fastapi entrypoint

---
+++ services/gateway/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+from services.gateway.api import api_router
+
+app = FastAPI()
+app.include_router(api_router)
+
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
