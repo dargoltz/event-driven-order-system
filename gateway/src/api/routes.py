fb2f8cc33d55070c701d8bb683651f4c25c42262
andrey.lesnov <andreythecoolguy@gmail.com>
2026-02-18 19:04:35 +0300

    feat: add usages order rpc client in api router

--- gateway/src/api/routes.py
+++ gateway/src/api/routes.py
@@ -1,9 +1,10 @@
 import uuid
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from .models import OrderCreateRequest, OrderResponse
+from .models import OrderResponse
 from ..core import ApiKeyHeader
+from ..grpc import order_rpc_client
 
 api_router = APIRouter(prefix="/api")
 
@@ -12,10 +13,12 @@ orders_router = APIRouter(prefix="/orders", tags=["orders"])
 
 @orders_router.post("/api/orders/")
 async def create_order(
-    request: OrderCreateRequest,
+    amount: float,
     _: ApiKeyHeader
-) -> OrderResponse:  # todo быстро либо будет происходить создание? стоит ли ждать и возвращать респонс?
-    ...
+) -> OrderResponse:
+    result = await order_rpc_client.create_order(amount)
+
+    return ...
 
 
 @orders_router.get("/api/orders/{order_id:uuid}")
@@ -23,15 +26,20 @@ async def get_order(
     order_id: uuid.UUID,
     _: ApiKeyHeader
 ) -> OrderResponse:
-    ...
+    result = await order_rpc_client.get_order(order_id)
+
+    if not result:
+        raise HTTPException(status_code=404, detail="Order not found")
+
+    return ...
 
 
 @orders_router.post("/api/orders/{order_id:uuid}/cancel")
 async def cancel_order(
     order_id: uuid.UUID,
     _: ApiKeyHeader
-):  # todo что стоит тут вернуть? сразу отмененный из бд? в сервисах не факт что отменится в этот момент еще
-    ...
+):
+    return await order_rpc_client.cancel_order(order_id)
 
 
 api_router.include_router(orders_router)
